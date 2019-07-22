df41c9c6e68d01ff7a5d4b62ff056f2b681d324a
Artyom Ivanov <artem_appstore@mail.ru>
2019-07-22 20:57:47 +0300

    First stable version of the libary. Main functions are tested.

--- src/main/java/com/artyom/mtx/RealMatrix.java
+++ src/main/java/com/artyom/mtx/RealMatrix.java
@@ -1,29 +1,24 @@
 package com.artyom.mtx;
 
-import java.util.ArrayList;
-import java.util.stream.IntStream;
+import com.sun.org.apache.regexp.internal.RE;
 
-public class AbstractMatrix<E extends MatrixElement> {
+import java.util.ArrayList;
+
+public class RealMatrix {
     private int rowCount;
     private int columnCount;
 
-    private ArrayList<ArrayList<E>> matrixElements;
+    private double[][] matrixElements;
 
-    public AbstractMatrix(int rows, int columns) {
+    public RealMatrix(int rows, int columns) {
         this.rowCount = rows;
         this.columnCount = columns;
 
-        matrixElements = new ArrayList<>(rows);
-        for (int i = 0; i < this.rowCount; i++) {
-            // need to set rows and columns by hand
-            matrixElements.add(new ArrayList<>(columns));
-            for (int j = 0; j < this.columnCount; j++)
-                matrixElements.get(i).add(null);
-        }
+        matrixElements = new double[rows][columns];
     }
 
-    public AbstractMatrix() {
-        this(0, 0);
+    public RealMatrix() {
+        this(1, 1);
     }
 
     public int getRowCount() {
@@ -34,35 +29,35 @@ public class AbstractMatrix<E extends MatrixElement> {
         return columnCount;
     }
 
-    /*public ArrayList<ArrayList<E>> getMatrixElements() {
-        return matrixElements;
-    }*/
+    public boolean isSquareMatrix() {
+        return rowCount == columnCount;
+    }
 
-    public void setElement(int row, int column, E newValue) {
+    public void setElement(int row, int column, double newValue) {
         if (row >= 0 && row < rowCount && column >= 0 && column < columnCount) {
-            matrixElements.get(row).set(column, newValue);
+            matrixElements[row][column] = newValue;
         } else {
             throw new RuntimeException("Incorrect values of indices!");
         }
     }
 
-    public E getElement(int row, int column) {
+    public double getElement(int row, int column) {
         if (row >= 0 && row < rowCount && column >= 0 && column < columnCount) {
-            return matrixElements.get(row).get(column);
+            return matrixElements[row][column];
         } else {
             throw new RuntimeException("Incorrect values of indices!");
         }
     }
 
-    public AbstractMatrix<E> add(AbstractMatrix<E> anotherMatrix) {
+    public RealMatrix add(RealMatrix anotherMatrix) {
         if (rowCount != anotherMatrix.rowCount || columnCount != anotherMatrix.columnCount)
             throw new RuntimeException("Sizes of two matrices must be same!");
         else {
-            AbstractMatrix<E> matrixSum = new AbstractMatrix<>(rowCount, columnCount);
+            RealMatrix matrixSum = new RealMatrix(rowCount, columnCount);
 
             for (int i = 0; i < rowCount; i++) {
                 for (int j = 0; j < columnCount; j++) {
-                    matrixSum.setElement(i, j, (E) this.getElement(i, j).add(anotherMatrix.getElement(i, j)));
+                    matrixSum.matrixElements[i][j] = this.matrixElements[i][j] + anotherMatrix.matrixElements[i][j];
                 }
             }
 
@@ -70,19 +65,19 @@ public class AbstractMatrix<E extends MatrixElement> {
         }
     }
 
-    public AbstractMatrix<E> multiply(AbstractMatrix<E> anotherMatrix) {
+    public RealMatrix multiply(RealMatrix anotherMatrix) {
         if (columnCount != anotherMatrix.rowCount) {
             throw new RuntimeException("Number of columns of the 1st matrix must be equal to number of rows of the 2nd matrix!");
         } else {
-            AbstractMatrix<E> prodMatrix = new AbstractMatrix<>(rowCount, anotherMatrix.columnCount);
+            RealMatrix prodMatrix = new RealMatrix(rowCount, anotherMatrix.columnCount);
+            double combination;
             for (int i = 0; i < rowCount; i++) {
                 for (int j = 0; j < anotherMatrix.columnCount; j++) {
-                    E prodOfElements = (E) this.getElement(i, 0).multiply(anotherMatrix.getElement(0, j));
-                    prodMatrix.setElement(i, j, prodOfElements);
-                    for (int k = 1; k < columnCount; k++) {
-                        prodOfElements = (E) this.getElement(i, k).multiply(anotherMatrix.getElement(k, j));
-                        prodMatrix.setElement(i, j, (E)prodOfElements.add(prodOfElements));
+                    combination = 0.0;
+                    for (int k = 0; k < columnCount; k++) {
+                        combination += this.matrixElements[i][k] * anotherMatrix.matrixElements[k][j];
                     }
+                    prodMatrix.matrixElements[i][j] = combination;
                 }
             }
 
@@ -90,59 +85,90 @@ public class AbstractMatrix<E extends MatrixElement> {
         }
     }
 
-    public AbstractMatrix<E> scalarMultiply(E scalar) {
-        AbstractMatrix<E> matrixScalMult = new AbstractMatrix<>(rowCount, columnCount);
+    public RealMatrix scalarMultiply(double scalar) {
+        RealMatrix matrixScalMult = new RealMatrix(rowCount, columnCount);
 
         for (int i = 0; i < rowCount; i++)
             for (int j = 0; j < columnCount; j++)
-                matrixScalMult.setElement(i, j, (E) this.getElement(i, j).multiply(scalar));
+                matrixScalMult.matrixElements[i][j] = this.matrixElements[i][j] * scalar;
 
         return matrixScalMult;
     }
 
-    public E getTrace() {
-        if (rowCount != columnCount)
+    public double getTrace() {
+        if (!this.isSquareMatrix())
             throw new RuntimeException("Trace is applied only for square matrices!");
         else {
-            MatrixElement sumOfDiagonal = this.getElement(0, 0);
-            for (int i = 1; i < rowCount; i++)
-                sumOfDiagonal = sumOfDiagonal.add(this.getElement(i, i));
+            double sumOfDiagonal = 0.0;
+            for (int i = 0; i < rowCount; i++)
+                sumOfDiagonal += this.matrixElements[i][i];
 
-            return (E) sumOfDiagonal;
+            return sumOfDiagonal;
         }
     }
 
-    public E getDeterminant() {
-        return null;
+    public double getDeterminant() {
+        return getDeterminantOf(this.matrixElements);
     }
 
-    public AbstractMatrix<E> transpose() {
+    public RealMatrix transpose() {
         int rowsTransp = this.columnCount;
         int columnsTransp = this.rowCount;
 
 
-        AbstractMatrix<E> transpMatrix = new AbstractMatrix<>(rowsTransp, columnsTransp);
+        RealMatrix transpMatrix = new RealMatrix(rowsTransp, columnsTransp);
         for (int i = 0; i < rowsTransp; i++)
             for (int j = 0; j < columnsTransp; j++)
-                transpMatrix.setElement(i, j, this.getElement(j, i));
+                transpMatrix.matrixElements[i][j] = matrixElements[j][i];
 
         return transpMatrix;
     }
 
-    public AbstractMatrix<E> getCofactorMatrix() {
-        return null;
+    public RealMatrix getCofactorMatrix() {
+        if (!this.isSquareMatrix()) {
+            throw new RuntimeException("Cofactor is applied only for the square matrices!");
+        } else {
+            RealMatrix cofactorMatrix = new RealMatrix(rowCount, columnCount);
+            for (int i = 0; i < rowCount; i++)
+                for (int j = 0; j < columnCount; j++)
+                    cofactorMatrix.setElement(i, j, getCofactorOf(this.matrixElements, i, j));
+
+            return cofactorMatrix;
+        }
     }
 
-    public AbstractMatrix<E> adjugate() {
+    public RealMatrix adjugate() {
         return this.getCofactorMatrix().transpose();
+    }
+
+    public RealMatrix getInverseMatrix() throws UnsupportedOperationException {
+        if (!this.isSquareMatrix()) {
+            throw new RuntimeException("Cofactor is applied only for the square matrices!");
+        } else {
+            double det = this.getDeterminant();
+            if (Math.abs(det) < 1e-6) {
+                throw new UnsupportedOperationException("The inverse matrix exists only for square matrix with non-zero determinant");
+            } else {
+                return this.adjugate().scalarMultiply(1.0 / det);
+            }
+        }
+    }
+
+    public static RealMatrix getEqualityMatrix(int rows) {
+        RealMatrix eqMatrix = new RealMatrix(rows, rows);
+        for (int i = 0; i < rows; i++) {
+            eqMatrix.setElement(i, i, 1.0);
+        }
+
+        return eqMatrix;
     }
 
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder(2 * rowCount * columnCount);
 
-        for (ArrayList<E> row : matrixElements) {
-            for (E element : row) {
+        for (double[] row : matrixElements) {
+            for (double element : row) {
                 sb.append(element);
                 sb.append(" ");
             }
@@ -150,5 +176,76 @@ public class AbstractMatrix<E extends MatrixElement> {
         }
 
         return sb.toString();
+    }
+
+    // rowMinor and columnMinor starts with 0
+    private static double[][] getMatrixOfMinor(double[][] matrix, int rowMinor, int columnMinor) {
+        int rows = matrix.length;
+        int columns = matrix[0].length;
+
+        int i1 = 0;
+        int j1 = 0;
+
+        double[][] matrixMinor = new double[rows - 1][columns - 1];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                if (i != rowMinor && j != columnMinor) {
+                    matrixMinor[i1][j1] = matrix[i][j];
+                    j1++;
+
+                    // then we should fill next row of the matrixMinor
+                    if (j1 == columns - 1) {
+                        j1 = 0;
+                        i1++;
+                    }
+                }
+            }
+        }
+
+        return matrixMinor;
+    }
+
+    private static double getDeterminantOf(double[][] matrix) {
+        int rows = matrix.length;
+        int columns = matrix[0].length;
+
+        if (rows != columns) {
+            throw new UnsupportedOperationException("Determinant is applied only for square matrices!");
+        } else {
+            switch (rows) {
+                case 1:
+                    return matrix[0][0];
+                case 2:
+                    return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0];
+                default:
+                    final int firstRow = 0;
+                    final double eps = 1e-5;
+                    double det = 0.0;
+                    double curElem;
+                    double[][] matrixOfMinor;
+                    // -1 in power of sum of i-th row and j-th column (if starting with 1)
+                    int koeff = 1;
+
+                    for (int i = 0; i < rows; i++) {
+                        curElem = matrix[firstRow][i];
+                        // if it isn't equal to zero, then next addend in det isn't zero
+                        if (Math.abs(curElem) >= eps) {
+                            matrixOfMinor = getMatrixOfMinor(matrix, firstRow, i);
+                            det += koeff * curElem * getDeterminantOf(matrixOfMinor);
+                        }
+                        // change sign every time
+                        koeff *= -1;
+                    }
+
+                    return det;
+            }
+        }
+    }
+
+    private static double getCofactorOf(double[][] matrix, int row, int column) {
+        double minor = getDeterminantOf(getMatrixOfMinor(matrix, row, column));
+
+        return Math.pow(-1.0, (row + column + 2)) * minor;
     }
 }
--- src/test/java/com/artyom/mtx/RealMatrixTest.java
+++ src/test/java/com/artyom/mtx/RealMatrixTest.java
@@ -4,12 +4,12 @@ import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import java.util.ArrayList;
-
 
 public class RealMatrixTest extends Assert {
     private static RealMatrix firstMatrix;
     private static RealMatrix secondMatrix;
+    private static RealMatrix eqMatrix;
+    private static final double eps = 1e-5;
 
     @BeforeClass
     public static void initTestMatrices() {
@@ -18,6 +18,9 @@ public class RealMatrixTest extends Assert {
 
         firstMatrix = new RealMatrix(rows, columns);
         secondMatrix = new RealMatrix(rows, columns);
+
+        // set to the equality matrix
+        eqMatrix = RealMatrix.getEqualityMatrix(rows);
     }
 
     @Test
@@ -25,13 +28,13 @@ public class RealMatrixTest extends Assert {
         int rows = firstMatrix.getRowCount();
 
         for (int i = 0; i < rows; i++) {
-            firstMatrix.setElement(i, i, new Real(1.0));
+            firstMatrix.setElement(i, i, 5.0);
         }
     }
-	
-	@Test
+
+    @Test
     public void testAdd() {
-        AbstractMatrix sumMatrix =  firstMatrix.add(secondMatrix);
+        RealMatrix sumMatrix = firstMatrix.add(secondMatrix);
 
         int rows = sumMatrix.getRowCount();
         int columns = sumMatrix.getColumnCount();
@@ -41,31 +44,93 @@ public class RealMatrixTest extends Assert {
         assertEquals(rows, secondMatrix.getRowCount());
         assertEquals(columns, secondMatrix.getColumnCount());
 
+        double eps = 1e-5;
         for (int i = 0; i < rows; i++) {
-            for(int j = 0; j <columns; j++) {
-                Real sum = (Real) firstMatrix.getElement(i, j).add(secondMatrix.getElement(i, j));
-                assertEquals(sumMatrix.getElement(i, j), sum); // ovveride equals for MatrixElement
+            for (int j = 0; j < columns; j++) {
+                double sum = firstMatrix.getElement(i, j) + (secondMatrix.getElement(i, j));
+                assertEquals(sum, sumMatrix.getElement(i, j), eps); // ovveride equals for MatrixElement
             }
         }
     }
 
     @Test
     public void testScalarMultiply() {
-        Real multiplier = new Real(3.0);
+        double multiplier = 3.0;
 
-        for (int i = 0; i < firstMatrix.getRowCount(); i++) {
-            firstMatrix.setElement(i, i, new Real(1.0));
-        }
-
-        AbstractMatrix scalarMultiplyMatrix =  firstMatrix.scalarMultiply(multiplier);
+        RealMatrix scalarMultiplyMatrix = firstMatrix.scalarMultiply(multiplier);
 
         int rows = scalarMultiplyMatrix.getRowCount();
         int columns = scalarMultiplyMatrix.getColumnCount();
 
+        double eps = 1e-5;
         for (int i = 0; i < rows; i++) {
-            for(int j = 0; j <columns; j++) {
-                assertEquals(scalarMultiplyMatrix.getElement(i, j), firstMatrix.getElement(i,j).multiply(multiplier)); // ovveride equals for MatrixElement
+            for (int j = 0; j < columns; j++) {
+                assertEquals(firstMatrix.getElement(i, j) * multiplier, scalarMultiplyMatrix.getElement(i, j), eps); // ovveride equals for MatrixElement
             }
         }
+    }
+
+    @Test
+    public void testProduct() {
+        RealMatrix prodMatrix1 = eqMatrix.multiply(secondMatrix);
+
+        int rows = prodMatrix1.getRowCount();
+        int columns = prodMatrix1.getColumnCount();
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                assertEquals(secondMatrix.getElement(i, j), prodMatrix1.getElement(i, j), eps); // ovveride equals for MatrixElement
+            }
+        }
+    }
+
+    @Test
+    public void testDet() {
+        double detEqMatrix = eqMatrix.getDeterminant();
+        assertEquals(1.0, detEqMatrix, eps);
+
+        int rows = firstMatrix.getRowCount();
+        int columns = firstMatrix.getColumnCount();
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                firstMatrix.setElement(i, j, (i + 1) * j + 1);
+            }
+        }
+
+        double detZero = firstMatrix.getDeterminant();
+        assertEquals(0.0, detZero, eps);
+    }
+
+    @Test
+    public void testInverseMatrix() {
+        int rows = secondMatrix.getRowCount();
+        int columns = secondMatrix.getColumnCount();
+
+        for(int i = 0; i < rows; i++) {
+            for(int j = 0; j < columns; j++) {
+                if(i == j)
+                    secondMatrix.setElement(i, j, i*j);
+                else
+                    secondMatrix.setElement(i,j,1);
+            }
+        }
+
+        try {
+            RealMatrix inverseMatrix = secondMatrix.getInverseMatrix();
+            // the product of matrix and its inverse is equality matrix
+            RealMatrix prod = secondMatrix.multiply(inverseMatrix);
+            RealMatrix prod2 = inverseMatrix.multiply(secondMatrix);
+
+            for(int i = 0; i < rows; i++) {
+                for (int j = 0; j < columns; j++) {
+                    assertEquals(eqMatrix.getElement(i, j), prod.getElement(i, j), eps);
+                    assertEquals(eqMatrix.getElement(i, j), prod2.getElement(i, j), eps);
+                }
+            }
+        } catch (UnsupportedOperationException uoe) {
+            uoe.printStackTrace();
+        }
+
     }
 }
